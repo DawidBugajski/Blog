d01d6d96849b4d2113c3309f1722ad38bd209f49
Dawid <dawidbugajski1996@gmail.com>
2023-05-30 13:26:39 +0200

    change base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Blog/',
+  base: '/Blog',
   resolve: {
     alias: {
       src: '/src',
